2962b28be08d289b99f9b0a01c8fb15abc03b16d
Will Bainton <baintonw@gmail.com>
2020-10-06 22:02:14 -0400

    added react-chat-widget, at step 3 of tutorial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
+//React Router
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+//React Chat Widget
+import { Widget, addResponseMessage } from 'react-chat-widget';
+ 
+import 'react-chat-widget/lib/styles.css';
 
 //Pages
 import Home from './Pages/Home'
@@ -16,6 +22,15 @@ import Layout from './Containers/Layout'
 import SignIn from './Components/SignIn'
 
 function App() {
+  useEffect(() => {
+    addResponseMessage('Hello, welcome to Food Rescued! How can I help you today?');
+  }, []);
+  //Message Handler for message widget
+  const handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Now send the message throught the backend API
+    // addResponseMessage(response) <-- Comment this in after back end is linked up
+  };
   return (
     
       <Router>
@@ -31,8 +46,11 @@ function App() {
                   <Home></Home>
             </Route>
           </Switch>
+          <Widget
+            handleNewUserMessage={handleNewUserMessage}
+          >
+          </Widget>
         </div>
-
       </Router>
     
   )
